053f5c1c2e66f07d444b5981eb3490097450a873
hardipgajera <gajerahardip789@gmail.com>
2022-01-25 00:58:25 +0530

    Initial Commit

---
+++ Extension-General/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Extension-General
+//
+//  Created by hardip gajera on 25/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Extension-General/Extension_GeneralApp.swift
@@ -0,0 +1,17 @@
+//
+//  Extension_GeneralApp.swift
+//  Extension-General
+//
+//  Created by hardip gajera on 25/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Extension_GeneralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
